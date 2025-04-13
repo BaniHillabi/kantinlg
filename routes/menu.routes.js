ea074f9f992c882e1759221e78ee026d5b83cece
aca <112835299+harfley@users.noreply.github.com>
2025-04-13 13:29:27 +0700

    Add files via upload

---
+++ routes/menu.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const menuController = require('../controllers/menu.controller');
+const auth = require ('../auth/auth')
+const upload = require('../utils/uploads');
+
+// Menambahkan menu
+app.post('/addmenu', upload.single("foto"), menuController.addMenu);
+
+
+// Melihat semua meneu
+app.get('/:search', menuController.getMenu);
+app.post('/', upload.single("foto"), menuController.addMenu);
+app.get('/', menuController.getAllMenus);
+app.put('/:id', menuController.updateMenu);
+app.delete('/:id', menuController.deleteMenu);
+
+// // Mendapatkan menu 
+// app.get('/:id', menuController.getMenuById);
+
+// // Mengupdate menu
+// app.put('/:id', menuController.updateMenu);
+
+// // Menghapus menu
+// app.delete('/:id', menuController.deleteMenu);
+
+
+module.exports = app;
---
+++ routes/siswa.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const siswaController = require('../controllers/siswa.controller');
+const auth = require('../auth/auth');
+const upload = require('../utils/uploads');
+
+
+app.post('/register',  upload.single("foto"), siswaController.registerSiswa); 
+app.post('/login', siswaController.loginSiswa); 
+app.put("/:id", siswaController.updateSiswa);      
+app.get("/:id", siswaController.getSiswa);
+// app.post("/order", auth.authVerify, siswaController.siswaOrder);
+
+// app.get('/menu', siswaController.getMenu);           
+// app.post('/order', auth.authVerify, siswaController.placeOrder); 
+// app.get('/order-status', auth.authVerify, siswaController.getOrderStatus); 
+// app.get('/order-history', auth.authVerify, siswaController.getOrderHistory); 
+// app.get('/print-invoice/:id', auth.authVerify, siswaController.printInvoice); 
+
+module.exports = app;
